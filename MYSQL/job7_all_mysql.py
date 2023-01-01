ec81129df7ad6852f302c1b979d2565933f9510a
Noahozo <117925977+Noahozo@users.noreply.github.com>
2023-01-01 13:55:44 +0800

    Update job7_all_mysql.py

--- MYSQL/job7_all_mysql.py
+++ MYSQL/job7_all_mysql.py
@@ -9,7 +9,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 username = 'root'  # 資料庫帳號
-password = 'Passw0rd!'  # 資料庫密碼
+password = '*****!'  # 資料庫密碼
 host = 'localhost'  # 資料庫位址
 port = '3306'  # 資料庫埠號
 database = 'sql_tutorial'  # 資料庫名稱
